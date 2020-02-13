c0a4706d52017e93b03eaff91ac1f4be076b5dee
Darya <Darya@PC-DARYA>
2020-02-13 21:00:08 +0100

    Avances ejercicio. Casi Terminado

--- PracticaJavaEE/src/Inicio.java
+++ PracticaJavaEE/src/Inicio.java
@@ -1,36 +1,57 @@
 import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-@WebServlet("/Inicio")
 public class Inicio extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Inicio() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private static final long serialVersionUID = 1L;	
+	private String user;
+	private String password;
+	private ServletContext context;
+	String nextPage = "";
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+
+		context = config.getServletContext();
+		user = context.getInitParameter("usuario");
+		password = context.getInitParameter("password");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	
+		HttpSession session = request.getSession(true);
 
+		String result = session.getAttribute("user").toString();
+
+		if (result != null) {
+			nextPage = "/registrado.jsp";
+		} else {
+			String usuario = request.getParameter("usuario");
+			String contraseña = request.getParameter("password");
+			
+			if (usuario != null && contraseña != null) {
+				
+				if (usuario == "darya" && contraseña == "darya") {
+					nextPage = "/registrado.jsp";
+				} else {
+					nextPage = "/autorizar.html";
+				}				
+			}
+			else {
+				nextPage = "/autorizar.html";
+			}
+		}
+
+		ServletContext servletContext = getServletContext();
+		RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher(nextPage);
+		requestDispatcher.forward(request, response);
+	}
 }
